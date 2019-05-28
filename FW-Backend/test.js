38775072bdc0ad425efc63c6fb9c4e46e8f9ba6a
PoojaRavichandran28 <poojaravishanthi@gmail.com>
2019-05-28 08:39:01 +0530

    Updated CRD

--- FW-Backend/test.js
+++ FW-Backend/test.js
@@ -1,11 +1,13 @@
+var process = require('process')
 var dataStore = require('./classCreation')
-var input = new dataStore('./file-store/',120,{"name": "Ram","age": "22","contact": "8524945848"})
 
-//input.create()
-async function execute(){
-    await input.create()
-    await input.read().then((res)=>console.log(res,'Completed read operation'))
-  //  await input.delete().then(()=>console.log('Completed delete operation'))
-}
-
-execute()
+if (!process.argv[2] || !process.argv[3] || !process.argv[4]) {
+    console.error('Commond doesnot exists')
+} else {
+     (async() => {
+        var input = new dataStore(process.argv[3], process.argv[4], process.argv[5])
+        if(process.argv[2] === 'create') await input.create() // To run the code use the command "npm test create path key value"
+        if(process.argv[2] === 'read') await input.read().then((res) => console.log(res))// To run the code use the command "npm test read path key"
+        if(process.argv[2] === 'delete')await input.delete().then(()=>console.log('Completed delete operation'))// To run the code use the command "npm test delete path key"
+    })()
+}  
